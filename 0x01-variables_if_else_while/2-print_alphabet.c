5b5e25d7f67505f46c8b9c668c80dcf256b1793c
BlessedF <faitholotu196@gmail.com>
2023-10-06 15:55:11 -0700

    Print alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	C = 'a';
+	while
+		(C <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
