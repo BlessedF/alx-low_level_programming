c835b7bfde77879788575fc31e505c04209fd2f7
BlessedF <faitholotu196@gmail.com>
2023-10-26 07:01:57 -0700

    0-putchar

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- *  * _putchar - Writes a character to the standard output (stdout)
- *   * @c: The character to be written
+ *  * _puts_recursion - Writes a character to the standard output (stdout)
+ *   * @s: The character to be written
  *    *
- *     * Return: 1 on success, -1 on error.
+ *     * Return: nothing
  *      */
-int _putchar(char c) {
-	    return write(1, &c, 1);
-}
-
-/**
- *  * _puts_recursion - Prints a string, followed by a new line.
- *   * @s: The string to be printed
- *    */
-void _puts_recursion(char *s) {
-	    if (*s == '\0') {
-		            _putchar('\n'); /* Print a new line when the end of the string is reached */
-			            return;
-				        }
-	        _putchar(*s); /* Print the current character */
-		    _puts_recursion(s + 1); /* Move to the next character in the string and make a recursive call */
-}
-
+void _puts_recursion(char *s)
+	    {
+		    if (*s != '\0')
+		    {
+			    _putchar(*s);
+			    _putschar_recursion(s + 1);
+		    }
+		    if (*s == '\0')
+		    {
+			    _putchar('\n');
+		   }
