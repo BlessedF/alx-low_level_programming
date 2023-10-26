457ed72c2b1848bb069b587693285b6b3f99f8cd
BlessedF <faitholotu196@gmail.com>
2023-10-26 07:05:40 -0700

    0-recursions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,9 @@ void _puts_recursion(char *s)
 		    if (*s != '\0')
 		    {
 			    _putchar(*s);
-			    _putschar_recursion(s + 1);
+			    _puts_recursion(s + 1);
 		    }
 		    if (*s == '\0')
 		    {
 			    _putchar('\n');
-		   }
+		  }
