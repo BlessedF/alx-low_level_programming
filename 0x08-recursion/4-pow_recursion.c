b0108ac11311fa10f2f111131773d7436cba2083
BlessedF <faitholotu196@gmail.com>
2023-10-26 08:31:16 -0700

    recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - the power of anumber
+ *
+ * @x: number
+ * @y: power
+ *
+ * Return: the result of power
+ */
+int _pow_recursion (int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0 )
+	{
+		return (1);
+	}
+}
+ 
