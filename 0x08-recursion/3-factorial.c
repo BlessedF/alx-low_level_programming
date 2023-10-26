4199e55f612bdc191e75e3e0246635cc054dda88
BlessedF <faitholotu196@gmail.com>
2023-10-26 08:06:50 -0700

    4 factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - grt the factorial of a number
+ *
+ * @n: factor to factorialise
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return(1);
+	}
+	return (n);
+}
