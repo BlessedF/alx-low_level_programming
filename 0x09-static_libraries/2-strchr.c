5e7c4148f4d5dd72e7adb7e18f26de799cf0e1b1
BlessedF <faitholotu196@gmail.com>
2023-10-27 08:56:40 -0700

    commit

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - program entry point
+ * @s: input value
+ * @c: input value
+ * Return: Always 0 (SUccess)
+ */
+ char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; n[s] >= '\0'; a++)
+	{
+		if (s(a) == n)
+			return (&s[a]);
+	}
+	return (0);
+}
