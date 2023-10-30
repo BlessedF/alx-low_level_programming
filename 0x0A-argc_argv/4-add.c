8608cb58f5e0c81501dcdd06cb68fc5e5891c4c3
BlessedF <faitholotu196@gmail.com>
2023-10-30 07:49:34 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,14 +7,17 @@
  * @argv: array that contains the prog.. command line arg..
  * Return: (0) when successful
  */
+
 int main(int argc, char *argv[])
+
 {
 	int i, j, add = 0;
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (! isdigit(argv[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
