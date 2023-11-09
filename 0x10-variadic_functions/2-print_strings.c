1ee5129cce2e970a5fd21cda703da73d040d12e3
BlessedF <faitholotu196@gmail.com>
2023-11-09 07:42:20 -0800

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+/**
+ * print_strings -prints strings
+ * @separator: separatorn between two strings
+ * @n: number of strings to print
+ */
+void print_strings(const char *separator, const unsigned int n, ...);
+{
+	unsigned int i;
+	char *str;
+
+	va_list ptr_str:
+
+}
