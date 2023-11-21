00b2a982014ffd29b22e78721da1b3992f12fb82
BlessedF <faitholotu196@gmail.com>
2023-11-21 03:58:04 -0800

    more on singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@ include "lists.h"
 
 /**
  *  * listint_len - returns the number of elements in a linked lists
- *   * @h: linked list of type to traverse
+ *   * @h: linked list of type listint_t to traverse
  *    *
  *     * Return: number of nodes
  *      */
